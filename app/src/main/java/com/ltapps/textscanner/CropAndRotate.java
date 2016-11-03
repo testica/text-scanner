15898ed2807b10b4d19f9157984d211ea9289ddc
testica <leitotesta@gmail.com>
2016-11-03 03:32:07 -0400

    CropAndRotate activity and layout created

---
+++ app/src/main/java/com/ltapps/textscanner/CropAndRotate.java
@@ -0,0 +1,22 @@
+package com.ltapps.textscanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by User on 03-11-2016.
+ */
+public class CropAndRotate extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crop_and_rotate);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
